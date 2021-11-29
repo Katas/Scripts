a2b9d92346087f02981b8041f1c088f314262dea
Katas <katas@users.noreply.github.com>
2021-11-29 15:57:19 +0000

    Added a simple dict cache script

---
+++ cached_dict.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Cache simple dict lookup.
+# This way, when I look up a word multiple times, the command won't have to fetch the same definition repeatedly, which makes it a bit faster.
+# Also pipes the definition to "less", just in case.
+
+tempdir="/tmp/dictemp"
+searchterm="$1"
+
+[ ! -d "$tempdir" ] && mkdir "$tempdir"
+
+if [ -z "$searchterm" ]; then
+	echo "Enter a search term" >&2
+	exit 1
+fi
+
+tempdef="$tempdir/$searchterm"
+if [ -f "$tempdef" ]; then
+	definition=$(cat "$tempdef")
+else
+	definition=$(dict "$searchterm")
+	if [ "$?" -ne 0 ]; then
+		exit 2
+	fi
+	echo "$definition" > "$tempdef"
+fi
+
+echo "$definition" | less
